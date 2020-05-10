3bba89e9eb246f6262d113ee6eda8adc4336c9a7
panda-with-issues <ryoda94@gmail.com>
2020-05-10 22:02:47 +0200

    create bot and add most of its functionality

--- bot.js
+++ bot.js
@@ -1,31 +1,64 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+const { ActivityHandler, ActivityTypes } = require('botbuilder');
+const fs = require('fs')
+const path = require('path')
+const Fabulous = require('./Fabulous')
 
-const { ActivityHandler, MessageFactory } = require('botbuilder');
+class Bot extends ActivityHandler {
+  constructor () {
+    super()
+    this.onMessage(async (context, next) => {
+      const msg = context.activity.text.split(' ')
+      if (msg[0] === '!ispirami') {
+        const name = context.activity.from.name
+        const card = Fabulous.draw()
+        const reply = { type: ActivityTypes.Message }
+        reply.text = `**${name}**, la mia parola per te è **${card.name}**`
+        reply.attachments = [await this.getImg(card)]
+        await context.sendActivity(reply)
+      }
 
-class EchoBot extends ActivityHandler {
-    constructor() {
-        super();
-        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
-        this.onMessage(async (context, next) => {
-            const replyText = `Echo: ${ context.activity.text }`;
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
-        });
+      else if (msg[0] === '!adunata') {
+          const name = context.activity.from.name
+          Fabulous.lady = name
+          await context.sendActivity(`Attenzione inutili servitor*, Sua Sfavillanza, la **Favolosah Signora ${name}**, richiede la vostra presenza!`)
+      }
 
-        this.onMembersAdded(async (context, next) => {
-            const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome!';
-            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-                if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
-                }
-            }
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
-        });
+      else if (msg[0] === '!occhiataccia' && context.activity.from.name === Fabulous.lady) {
+          const person = msg[msg.length - 1]
+          if (Fabulous.hasWarning(person)) {
+              Fabulous.reset()
+              await context.sendActivity(`Uh oh, ${person} aveva già un'occhiataccia... **La colpa è tutta sua!**`)
+          } else {
+              Fabulous.warn(person)
+              await context.sendActivity(`Attenzione **${person}**, Sua Sfavillanza ${Fabulous.lady} sospetta di te...`)
+          }
+      }
+
+      else if (msg[0] === '!stato') {
+          const warned = Fabulous.warned.map(person => {
+              return '**' + person + '**'
+          })
+          await context.sendActivity(`Le persone sospettate sono ${warned.join(', ')}.`)
+      }
+
+      else if (msg[0] === '!aiuto') {
+          // tanks for this markdown
+          await context.sendActivity('I comandi disponibili sono:**!adunata**: comincia un nuovo turno. Chi dà il comando interpreta la Favolosah Signora;\n **!ispirami**: intercede presso la Dea Arcobaleno per offrire uno spunto da usare per la propria scusa;\n **!occhiataccia [per giocatore]**: assegna un\'occhiataccia all* giocator* indicat*. Il round termina quando un* giocator* prende la seconda occhiataccia;\n **!stato**: mostra quali giocatori hanno già un\'occhiataccia;\n **!aiuto**: mostra questo messaggio')
+          await context.sendActivity( //TODO: Splittare le stringhe, debuggare !stato, aggiungere un counter per le ispirazioni
+      }
+      await next()
+    })
+  }
+
+    async getImg (card, context) {
+        const imageData = fs.readFileSync(path.join(__dirname, card.src));
+        const base64Image = Buffer.from(imageData).toString('base64');
+        return {
+            name: card.name + '.jpeg',
+            contentType: 'image/jpeg',
+            contentUrl: `data:image/jpeg;base64,${ base64Image }`
+        };
     }
 }
 
-module.exports.EchoBot = EchoBot;
+module.exports.Bot = Bot
