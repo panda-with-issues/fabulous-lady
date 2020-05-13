59144d9dab677d97752c61626c068642e46ba36a
panda-with-issues <ryoda94@gmail.com>
2020-05-13 10:29:22 +0200

    add GNU3 License

--- Deck.js
+++ Deck.js
@@ -1,6 +1,24 @@
 /**
+ * This module is required to bot.js
+ *
  * Imgs must have the same name of card name because of the attachement method I used in Bot.js
  * The largest dimension of the image must be <= 600px
+ *
+ * Copyright (C) 2020 Yuuki Gaudiuso
+ * gaudiuso.y@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 const Deck = [
